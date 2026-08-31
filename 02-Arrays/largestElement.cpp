8ecc743ebc6749181cc5b841cb46daa016555d30
Priyanshu Srivastava <priysri1210@gmail.com>
2026-08-31 21:23:30 +0530

    Add largest element array solution

---
+++ 02-Arrays/largestElement.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+        int largest = nums[0];
+        for(int i = 0; i<nums.size();i++){
+            if(nums[i]>largest){
+                largest = nums[i];
+            }
+        }
+        return largest;
+    }
+};
