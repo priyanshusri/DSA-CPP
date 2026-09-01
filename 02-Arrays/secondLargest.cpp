6398090b4652c2a68f15957dbd1a9f39f19af4eb
Priyanshu Srivastava <priysri1210@gmail.com>
2026-09-01 21:43:22 +0530

    Add second largest element solution

---
+++ 02-Arrays/secondLargest.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int secondLargestElement(vector<int>& nums) {
+        //your code goes here
+      int largest = INT_MIN;
+      int secondLargest = INT_MIN;//smallest negative signed integer value
+      for(int i = 0;i<nums.size();i++){
+        if(nums[i]>largest){
+        secondLargest = largest;
+        largest = nums[i];
+        }
+        else if(nums[i]>secondLargest && nums[i]!=largest){
+            secondLargest = nums[i];
+        }
+      }
+      if(secondLargest == INT_MIN){
+        return -1;
+      }
+      return secondLargest;
+    }
+};
