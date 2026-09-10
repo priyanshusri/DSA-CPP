ee38c59ab3b804d500b8fae19a8411a3d073dc09
Priyanshu Srivastava <priysri1210@gmail.com>
2026-09-10 21:28:34 +0530

    Added optimal solution for the problem

--- 02-Arrays/majorityElement.cpp
+++ 02-Arrays/majorityElement.cpp
@@ -16,3 +16,27 @@ public:
        return {}; 
     }
 };
+//Boyer-moore algorithm approach(optimal)
+//Time: O(n), Space: O(1)
+/*
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ele = 0;
+        int count = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(count == 0){
+                count = 1;
+                ele = nums[i];
+            }
+            else if(nums[i]==ele){
+                count++;
+            }
+            else{
+                count--;
+            } 
+        }
+        return ele;
+    }
+};
+*/
