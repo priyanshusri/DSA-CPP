435690a5f8249c8ad1970fe26b32b1b47b546813
Priyanshu Srivastava <priysri1210@gmail.com>
2026-09-23 22:37:09 +0530

    Add first unique character solution

---
+++ 03-Strings-Hashing/firstUniqChar.cpp
@@ -0,0 +1,18 @@
+//Time: O(n), Space: O(1)
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int count[26]={0};
+        for(int i=0;i<s.length();i++){
+            count[s[i]-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(count[s[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
